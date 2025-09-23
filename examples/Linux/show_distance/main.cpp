2c13568a1537e2d78bd2ce82248e11b76247d34d
Larry Bank <bitbank@pobox.com>
2025-09-23 13:44:25 +0100

    fixed Linux example working

--- examples/Linux/show_distance/main.cpp
+++ examples/Linux/show_distance/main.cpp
@@ -18,8 +18,8 @@ int main(int argc, char *argv[])
 {
 int i;
 
-    printf("bb_proximity show distance example\n");
-    printf("Initializes, configures, then loops displaying the distance.\n");
+        printf("bb_proximity show distance example\n");
+        printf("Initializes, configures, then loops displaying the distance.\n");
 	// I2C bus 1 is the default on RPI hardware
         // Other Linux systems can use any number from 0 to 10 (usually)
         i = bbp.init(4); // find a supported RTC
@@ -27,10 +27,16 @@ int i;
             printf("NB: by default your system may require root access for I2C\n");
 	    printf("No supported device found\n");
             return -1; // problem - quit
-        } else {
-            printf("Proximity sensor detected and initialized\n");
-            printf("device type = %s\n", szProxType[bbp.type()]);
+        }
+        printf("Proximity sensor detected and initialized\n");
+        printf("device type = %s\n", szProxType[bbp.type()]);
+	printf("The distance value is the number of detected photons\n");
+	printf("This is a non-linear value which is inversely related to the distance.\n");
+	bbp.start(BBP_CAPS_PROXIMITY); // start reading proximity values
+	while (1) {
+		int iPhotons = bbp.getProximity();
+		printf("Photons: %d\n", iPhotons);
+		usleep(250000); // show 4 samples per second
 	}
-
 return 0;
 } /* main() */
